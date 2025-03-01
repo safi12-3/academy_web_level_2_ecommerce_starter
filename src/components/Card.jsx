cf282715fbe339016a60aa8e8d63eba3709b18f9
safi12-3 <safihaw0@gmail.com>
2025-03-02 00:34:16 +0200

    new commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,9 @@ const Card = () => {
               className="w-full h-full object-cover"
             />
             <button className="absolute top-2 right-2 p-2 rounded-full bg-white shadow-md hover:bg-gray-100 cursor-pointer">
+
               <FaRegHeart className="w-5 h-5 text-gray-600" />
+
             </button>
           </div>
           <div className="p-4 flex flex-col gap-2">
